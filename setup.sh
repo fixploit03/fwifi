45266eb7340e9183a85181def5337aa80f28322b
rofidoang03 <149921715+rofidoang03@users.noreply.github.com>
2024-03-09 16:31:29 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,23 +1,60 @@
 #!/bin/bash
 
+g="\e[1;32m"
+r= "\e[0m"
+c="\e[1;36m"
+
 if [ "$EUID" -ne 0 ]; then
-  echo "[ERROR] Skrip ini harus dijalankan sebagai root."
+  echo "[ERROR] This script must be run as root."
   exit 1
 fi
 
+sleep 1
+echo -e "${r}[ ${c}info ${r}] Updating repository..."
+sleep 1
 apt-get update -y
+sleep 1
+echo -e "${r}[ ${g}OK ${r}] Repository successfully updated."
+sleep 1
+echo -e "${r}[ ${c}info ${r}] Upgrading repository..."
+sleep 1
 apt-get upgrade -y
+sleep 1
+echo -e "${r}[ ${g}OK ${r}] Repository successfully upgraded."
+sleep 1
+echo -e "${r}[ ${c}info ${r}] install git..."
+sleep 1
 apt-get install git -y
-git clone https://github.com/rofidoang03/wifi-hacking/
+sleep 1
+echo -e "${r}[ ${g}OK ]${r} git successfully installed."
+sleep 1
+echo -e "${r}[ ${c}info ${r}] Cloning fwifi repository..."
+sleep 1
+git clone https://github.com/rofidoang03/fwifi.git
+cd fwifi
+
+# Install aircrack-ng
 cd aircrack-ng
-chmod +x instal.sh
-./instal.sh
+chmod +x install.sh
+./install.sh
 cd ..
+
+# Install wordlists
 cd wordlists
-chmod +x instal.sh
-./instal.sh
+chmod +x install.sh
+./install.sh
 cd ..
+
+# Install rtl8188eus
 cd rtl8188eus
-chmod +x instal.sh
-./instal.sh
-echo "[+] Selesai."
+chmod +x install.sh
+./install.sh
+cd ..
+
+# Set permissions for fwifi
+chmod +x fwifi
+
+# Copy fwifi to /usr/bin
+cp fwifi /usr/bin
+
+echo -e "${r}[ ${g}OK ${r}] fwifi successfully installed."
